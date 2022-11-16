e5b53bdd15f94e52ff056d91959e65f26a365183
Carlos <carloscontato2000@hotmail.com>
2022-11-16 14:17:34 -0300

    Adicionando diversidade de backgrounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,18 @@ const relogio = setInterval(function time(){
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+
+    if (hr >= 5 && hr < 7) {
+        document.body.style.background =
+        "linear-gradient(120deg, #108dc7, #ef8e38)";
+    } else if (hr >= 7 && hr < 16) {
+        document.body.style.background =
+        "linear-gradient(120deg, #FDC830, #F37335)";
+    } else if (hr >= 16 && hr < 18) {
+        document.body.style.background =
+        "linear-gradient(120deg, #8A2387, #E94057, #F27121)";
+    } else {
+        document.body.style.background =
+        "linear-gradient(120deg, #0F2027, #203A43, #2c5364)";
+    }
 })
